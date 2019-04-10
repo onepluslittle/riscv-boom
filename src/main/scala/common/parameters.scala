6e0dc56e2d42764c0297f86cdc32d8ea50f3e4ac
Ben Korpan <bkorpan@berkeley.edu>
2019-04-09 17:37:40 -0700

    Option to disable fast wakeup broadcast to rename.

--- src/main/scala/common/parameters.scala
+++ src/main/scala/common/parameters.scala
@@ -43,6 +43,7 @@ case class BoomCoreParams(
    enableBrResolutionRegister: Boolean = true,
    enableCommitMapTable: Boolean = false,
    enableFastPNR: Boolean = true,
+   enableFastWakeupsToRename: Boolean = true,
    enableBTBContainsBranches: Boolean = true,
    enableBranchPredictor: Boolean = true,
    enableBTB: Boolean = true,
@@ -227,6 +228,7 @@ trait HasBoomCoreParameters extends freechips.rocketchip.tile.HasCoreParameters
    // Extra Knobs and Features
    val ENABLE_COMMIT_MAP_TABLE = boomParams.enableCommitMapTable
    val enableFastPNR = boomParams.enableFastPNR
+   val enableFastWakeupsToRename = boomParams.enableFastWakeupsToRename
 
    //************************************
    // Implicitly calculated constants
